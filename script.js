83645687fe40a1239a7734895e81bce0872878ee
Ophir LOJKINE <pere.jobs@gmail.com>
2017-05-18 23:40:47 +0300

    Refactor JS

--- script.js
+++ script.js
@@ -1,30 +1,42 @@
-const BASE = "https://api.github.com/repos/lovasoa/sha_hashes/contents/hashes/sha1";
-const form = document.querySelector("form");
-const response = document.querySelector("#response");
-
-function onsubmit(evt) {
-  evt.preventDefault();
-  var hash = form.elements.hash.value;
+/**
+ * Takes a password hash and returns a promise that resolves to the password
+ * @param {string} hash
+ * @returns {Promise<string>} password
+ **/
+function reverse_hash(hash) {
+  const BASE = "https://api.github.com/repos/lovasoa/sha_hashes/contents/hashes/sha1";
   var folders = hash.substring(0,4).split("").join("/");
   var url = BASE + '/' + folders + '/' + hash;
-  response.textContent = "Loading...";
-  fetch(url, {
+  const options = {
     headers: {
       "Accept": "application/vnd.github.VERSION.raw"
     }
-  }).then(r => r.ok ? r.text() : r.json().then(x=>Promise.reject(x)))
-    .then(handle).catch(error);
+  };
+  return fetch(url, options)
+    .then(r => r.ok ?
+                  r.text() :
+                  r.json().then(x => Promise.reject(x))
+    );
 }
 
-function handle(resp) {
-  response.className = "success";
-  response.textContent = resp;
+(function(){
+
+function onsubmit(evt) {
+  evt.preventDefault();
+  response.textContent = "Loading...";
+  function handle(resp) {
+    response.className = "success";
+    response.textContent = resp;
+  }
+  function error(resp) {
+    response.className = "error";
+    response.textContent = resp.message || JSON.stringify(resp);
+    console.error(resp);
+  }
+  reverse_hash(form.elements.hash.value).then(handle).catch(error);
 }
 
-function error(resp) {
-  response.className = "error";
-  response.textContent = resp.message || JSON.stringify(resp);
-  console.error(resp);
-}
-
+const form = document.querySelector("form");
+const response = document.querySelector("#response");
 form.addEventListener("submit", onsubmit);
+})();
